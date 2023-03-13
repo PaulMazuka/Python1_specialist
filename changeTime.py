b9657b1131770eb5acd7c52d0e9cc37be082a283
Paul Maz <akzary@gmail.com>
2023-03-13 22:00:00 +0300

    complete home work module 1

---
+++ changeTime.py
@@ -0,0 +1,8 @@
+seconds=int(input("Прошло секунд: "))
+day= seconds//86400
+hour= seconds//3600
+if  hour >= 24:
+    hour = hour - 24
+minutes= (seconds//60)%60
+last_seconds=seconds%60
+print( day," Дней ",hour," Часов ",minutes," минут",last_seconds," секунд")
